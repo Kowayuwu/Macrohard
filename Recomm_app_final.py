10cc99adee77bca80520faf32f8e21d9c8237238
Yagebu <yagebuxie@gmail.com>
2021-04-11 11:39:46 +1000

    final commit 4

--- Recomm_app_final.py
+++ Recomm_app_final.py
@@ -72,169 +72,173 @@ def login():
 
 	value_for_money_rating = None
 	def rating_input_submit(event):
-		if float(rating_input.get()) >= 1 and float(rating_input.get()) <= 5:
-			global value_for_money_rating
-			value_for_money_rating = rating_input.get()
-			# user_review_frame.destroy()
-			# the LHS frame needs to be destroyed as well
-			new_window = Toplevel(bg='grey50')
-			new_window.title('Your Review')
-			new_window.config(bg='dim grey')
+		try:
+			if float(rating_input.get()) >= 1 and float(rating_input.get()) <= 5:
+				global value_for_money_rating
+				value_for_money_rating = rating_input.get()
+				# user_review_frame.destroy()
+				# the LHS frame needs to be destroyed as well
+				new_window = Toplevel(bg='grey50')
+				new_window.title('Your Review')
+				new_window.config(bg='dim grey')
 
-			criteria_frame_location = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-			criteria_frame_location.grid(row=0, column=0, padx=10, pady=10)
-			criteria_frame_hospitality = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-			criteria_frame_hospitality.grid(row=0, column=1, padx=10, pady=10)
-			criteria_frame_cleaniness = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-			criteria_frame_cleaniness.grid(row=0, column=2, padx=10, pady=10)
-			criteria_frame_amenities = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-			criteria_frame_amenities.grid(row=0, column=3, padx=10, pady=10)
-			criteria_frame_EoB = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-			criteria_frame_EoB.grid(row=0, column=4, padx=10, pady=10)
+				criteria_frame_location = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+				criteria_frame_location.grid(row=0, column=0, padx=10, pady=10)
+				criteria_frame_hospitality = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+				criteria_frame_hospitality.grid(row=0, column=1, padx=10, pady=10)
+				criteria_frame_cleaniness = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+				criteria_frame_cleaniness.grid(row=0, column=2, padx=10, pady=10)
+				criteria_frame_amenities = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+				criteria_frame_amenities.grid(row=0, column=3, padx=10, pady=10)
+				criteria_frame_EoB = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+				criteria_frame_EoB.grid(row=0, column=4, padx=10, pady=10)
 
-			location_prompt = Label(criteria_frame_location, width=25, text='How satisfied were you with the \n location of the hotel?', anchor=W, bg='grey23', fg='white')
-			location_prompt.grid(row=0, column=0, padx=5, pady=5)
-			hospitality_prompt = Label(criteria_frame_hospitality, width=25, text="How satisfied were you with the \n hotel's hospitality?", anchor=W, bg='grey23', fg='white')
-			hospitality_prompt.grid(row=0, column=0, padx=5, pady=5)
-			cleaniness_prompt = Label(criteria_frame_cleaniness, width=25, text='How satisfied were you with the \n cleaniness of the hotel?', anchor=W, bg='grey23', fg='white')
-			cleaniness_prompt.grid(row=0, column=0, padx=5, pady=5)
-			amenities_prompt = Label(criteria_frame_amenities, width=25, text='How satisfied were you with the \n quality and range of amenities?', anchor=W, bg='grey23', fg='white')
-			amenities_prompt.grid(row=0, column=0, padx=5, pady=5)
-			EoB_prompt = Label(criteria_frame_EoB, width=25, text='How satisfied were you with the \n procedure for booking the hotel?', anchor=W, bg='grey23', fg='white')
-			EoB_prompt.grid(row=0, column=0, padx=5, pady=5)
-
-
-
-
-			# create radiobuttons
-			satisfaction_levels = [
-				("Very Satisfied", "VS", 1),
-				("Satisfied", "S", 2),
-				("Neutral", "N", 3),
-				("Unsatisfied", "U", 4),
-				("Very Unsatisfied", "VU", 5)
-			]
-
-			global var1
-			global var2
-			global var3
-			global var4
-			global var5
-			var1 = StringVar()
-			var1.set("VS")
-			var2 = StringVar()
-			var2.set("VS")
-			var3 = StringVar()
-			var3.set("VS")
-			var4 = StringVar()
-			var4.set("VS")
-			var5 = StringVar()
-			var5.set("VS")
-
-			for level, syms, row_num in satisfaction_levels:
-				Radiobutton(criteria_frame_location, text=level, variable=var1, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
-				Radiobutton(criteria_frame_hospitality, text=level, variable=var2, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
-				Radiobutton(criteria_frame_cleaniness, text=level, variable=var3, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
-				Radiobutton(criteria_frame_amenities, text=level, variable=var4, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
-				Radiobutton(criteria_frame_EoB, text=level, variable=var5, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
-
-			# gets all the symbols for whichever radiobutton is selected for each category
-			def submit_satisfaction():
-				global location_sym
-				global hospitality_sym
-				global cleaniness_sym
-				global amenities_sym
-				global EoB_sym
-				global location_num
-				global hospitality_num
-				global cleaniness_num
-				global amenities_num
-				global EoB_num
-
-				satisfaction_to_number_conversion = {
-					"VS": 2,
-					"S": 1,
-					"N": 0,
-					"U": -1, 
-					"VU": -2
-				}
-
-				location_sym = var1.get()
-				hospitality_sym = var2.get()
-				cleaniness_sym = var3.get()
-				amenities_sym = var4.get()
-				EoB_sym = var5.get()
-
-				location_num = satisfaction_to_number_conversion[var1.get()]
-				hospitality_num = satisfaction_to_number_conversion[var2.get()]
-				cleaniness_num = satisfaction_to_number_conversion[var3.get()]
-				amenities_num = satisfaction_to_number_conversion[var4.get()]
-				EoB_num = satisfaction_to_number_conversion[var5.get()]
-
-				#updates numerical values of user_profile following data entries
-				global user_profile
-				user_profile['location'] = user_profile['location'] + abs(location_num)
-				user_profile['hospitality'] = user_profile['hospitality'] + abs(hospitality_num)
-				user_profile['cleaniness'] = user_profile['cleaniness'] + abs(cleaniness_num)
-				user_profile['amenities'] = user_profile['amenities'] + abs(amenities_num)
-				user_profile['ease of booking'] = user_profile['ease of booking'] + abs(EoB_num)
-					
-				# print(user_profile)
-
-	# ------------------------- insert values into database and personal profile!
+				location_prompt = Label(criteria_frame_location, width=25, text='How satisfied were you with the \n location of the hotel?', anchor=W, bg='grey23', fg='white')
+				location_prompt.grid(row=0, column=0, padx=5, pady=5)
+				hospitality_prompt = Label(criteria_frame_hospitality, width=25, text="How satisfied were you with the \n hotel's hospitality?", anchor=W, bg='grey23', fg='white')
+				hospitality_prompt.grid(row=0, column=0, padx=5, pady=5)
+				cleaniness_prompt = Label(criteria_frame_cleaniness, width=25, text='How satisfied were you with the \n cleaniness of the hotel?', anchor=W, bg='grey23', fg='white')
+				cleaniness_prompt.grid(row=0, column=0, padx=5, pady=5)
+				amenities_prompt = Label(criteria_frame_amenities, width=25, text='How satisfied were you with the \n quality and range of amenities?', anchor=W, bg='grey23', fg='white')
+				amenities_prompt.grid(row=0, column=0, padx=5, pady=5)
+				EoB_prompt = Label(criteria_frame_EoB, width=25, text='How satisfied were you with the \n procedure for booking the hotel?', anchor=W, bg='grey23', fg='white')
+				EoB_prompt.grid(row=0, column=0, padx=5, pady=5)
 
 
-				# new page to write an optional review
-				criteria_frame_location.destroy()
-				criteria_frame_hospitality.destroy()
-				criteria_frame_cleaniness.destroy()
-				criteria_frame_amenities.destroy()
-				criteria_frame_EoB.destroy()
-				submit_btn.destroy()
-
-				optional_review_frame = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
-				optional_review_frame.grid(row=0, column=0, padx=10, pady=10)
-
-				review_prompt = Label(optional_review_frame, text='You can choose to write a short review for others to see', bg='grey23', fg='white')
-				review_prompt.grid(row=0, column=0, padx=10, pady=10)
-				review = Text(optional_review_frame, width=50, height=10, bg='grey70', fg='black', font=('Arial', 11))
-				review.grid(row=1, column=0, padx=10, pady=10)
-
-				def finish_review():
-					global review_response
-					review_response = ''
-					if len(review.get("1.0", END)) != 0:
-						review_response += review.get("1.0", END)
-						print(review_response)
-					optional_review_frame.destroy()
-					finish_btn.destroy()
-
-					new_window.config(bg='grey23')
-					thankyou_label = Label(new_window, text='Thank you\nYour response has been recorded', bg='grey23', fg='white')
-					thankyou_label.config(font=('Helvetica', 40))
-					thankyou_label.pack()
-					sat = {"VS": 5,"S": 4,"N": 3,"U": 2, "VU": 1}
-					final_list = [hotel_being_reviewed, username, sat.get(location_sym), sat.get(cleaniness_sym), sat.get(hospitality_sym), sat.get(EoB_sym), sat.get(amenities_sym), review_response]
-					print(final_list)
-					with open('ratings.csv', 'a') as f_object:
-						writer_object = writer(f_object)
-						writer_object.writerow(final_list)
-						f_object.close()
-					return final_list
-
-				finish_btn = Button(optional_review_frame, text='Finish', padx=10, pady=10, bg='grey50', fg='white', command=finish_review)
-				finish_btn.grid(row=4, column=0, padx=10, pady=10)
-
-			# create submit button
-			submit_btn = Button(new_window, text='Submit', padx=10, pady=10, bg='grey50', fg='white', command=submit_satisfaction)
-			submit_btn.config(font=("Italic", 11))
-			submit_btn.grid(row=1, column=0, columnspan=5)
 
 
-		# if input was not between 1&5
-		else:
+				# create radiobuttons
+				satisfaction_levels = [
+					("Very Satisfied", "VS", 1),
+					("Satisfied", "S", 2),
+					("Neutral", "N", 3),
+					("Unsatisfied", "U", 4),
+					("Very Unsatisfied", "VU", 5)
+				]
+
+				global var1
+				global var2
+				global var3
+				global var4
+				global var5
+				var1 = StringVar()
+				var1.set("VS")
+				var2 = StringVar()
+				var2.set("VS")
+				var3 = StringVar()
+				var3.set("VS")
+				var4 = StringVar()
+				var4.set("VS")
+				var5 = StringVar()
+				var5.set("VS")
+
+				for level, syms, row_num in satisfaction_levels:
+					Radiobutton(criteria_frame_location, text=level, variable=var1, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
+					Radiobutton(criteria_frame_hospitality, text=level, variable=var2, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
+					Radiobutton(criteria_frame_cleaniness, text=level, variable=var3, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
+					Radiobutton(criteria_frame_amenities, text=level, variable=var4, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
+					Radiobutton(criteria_frame_EoB, text=level, variable=var5, value=syms, bg='grey23', fg='white', activeforeground='white', activebackground='grey23', selectcolor='grey23').grid(row=row_num, column=0)
+
+				# gets all the symbols for whichever radiobutton is selected for each category
+				def submit_satisfaction():
+					global location_sym
+					global hospitality_sym
+					global cleaniness_sym
+					global amenities_sym
+					global EoB_sym
+					global location_num
+					global hospitality_num
+					global cleaniness_num
+					global amenities_num
+					global EoB_num
+
+					satisfaction_to_number_conversion = {
+						"VS": 2,
+						"S": 1,
+						"N": 0,
+						"U": -1, 
+						"VU": -2
+					}
+
+					location_sym = var1.get()
+					hospitality_sym = var2.get()
+					cleaniness_sym = var3.get()
+					amenities_sym = var4.get()
+					EoB_sym = var5.get()
+
+					location_num = satisfaction_to_number_conversion[var1.get()]
+					hospitality_num = satisfaction_to_number_conversion[var2.get()]
+					cleaniness_num = satisfaction_to_number_conversion[var3.get()]
+					amenities_num = satisfaction_to_number_conversion[var4.get()]
+					EoB_num = satisfaction_to_number_conversion[var5.get()]
+
+					#updates numerical values of user_profile following data entries
+					global user_profile
+					user_profile['location'] = user_profile['location'] + abs(location_num)
+					user_profile['hospitality'] = user_profile['hospitality'] + abs(hospitality_num)
+					user_profile['cleaniness'] = user_profile['cleaniness'] + abs(cleaniness_num)
+					user_profile['amenities'] = user_profile['amenities'] + abs(amenities_num)
+					user_profile['ease of booking'] = user_profile['ease of booking'] + abs(EoB_num)
+						
+					# print(user_profile)
+
+		# ------------------------- insert values into database and personal profile!
+
+
+					# new page to write an optional review
+					criteria_frame_location.destroy()
+					criteria_frame_hospitality.destroy()
+					criteria_frame_cleaniness.destroy()
+					criteria_frame_amenities.destroy()
+					criteria_frame_EoB.destroy()
+					submit_btn.destroy()
+
+					optional_review_frame = LabelFrame(new_window, padx=10, pady=10, bg='grey23')
+					optional_review_frame.grid(row=0, column=0, padx=10, pady=10)
+
+					review_prompt = Label(optional_review_frame, text='You can choose to write a short review for others to see', bg='grey23', fg='white')
+					review_prompt.grid(row=0, column=0, padx=10, pady=10)
+					review = Text(optional_review_frame, width=50, height=10, bg='grey70', fg='black', font=('Arial', 11))
+					review.grid(row=1, column=0, padx=10, pady=10)
+
+					def finish_review():
+						global review_response
+						review_response = ''
+						if len(review.get("1.0", END)) != 0:
+							review_response += review.get("1.0", END)
+							print(review_response)
+						optional_review_frame.destroy()
+						finish_btn.destroy()
+
+						new_window.config(bg='grey23')
+						thankyou_label = Label(new_window, text='Thank you\nYour response has been recorded', bg='grey23', fg='white')
+						thankyou_label.config(font=('Helvetica', 40))
+						thankyou_label.pack()
+						sat = {"VS": 5,"S": 4,"N": 3,"U": 2, "VU": 1}
+						final_list = [hotel_being_reviewed, username, sat.get(location_sym), sat.get(cleaniness_sym), sat.get(hospitality_sym), sat.get(EoB_sym), sat.get(amenities_sym), review_response.strip('\n')]
+						print(final_list)
+						with open('ratings.csv', 'a') as f_object:
+							writer_object = writer(f_object)
+							writer_object.writerow(final_list)
+							f_object.close()
+						return final_list
+
+					finish_btn = Button(optional_review_frame, text='Finish', padx=10, pady=10, bg='grey50', fg='white', command=finish_review)
+					finish_btn.grid(row=4, column=0, padx=10, pady=10)
+
+				# create submit button
+				submit_btn = Button(new_window, text='Submit', padx=10, pady=10, bg='grey50', fg='white', command=submit_satisfaction)
+				submit_btn.config(font=("Italic", 11))
+				submit_btn.grid(row=1, column=0, columnspan=5)
+
+
+			# if input was not between 1&5
+			else:
+				messagebox.showerror("Input Error", "The inputed rating was not between 1 & 5")
+
+		except:
 			messagebox.showerror("Input Error", "The inputed rating was not between 1 & 5")
-
+				
 	rating_input = Entry(user_review_frame, width=22)
 	rating_input.grid(row=3, column=1, padx=5, pady=5)
 	rating_input.config(fg='grey')
